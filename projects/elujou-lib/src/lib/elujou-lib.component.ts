85515600ad128bfdc95c9263f9153bd3cf87f6c7
kvartiil <138615442+kvartiil@users.noreply.github.com>
2024-06-15 16:32:42 +0300

    Initial commit

---
+++ projects/elujou-lib/src/lib/elujou-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-elujou-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      elujou-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class ElujouLibComponent {
+
+}
---
+++ projects/elujou-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of elujou-lib
+ */
+
+export * from './lib/elujou-lib.service';
+export * from './lib/elujou-lib.component';
